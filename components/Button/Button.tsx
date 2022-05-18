d623fd32e2cfb691950875eba5c80295c32995f8
Dmytro <ihnatenko.dev@gmail.com>
2022-05-18 19:42:38 +0300

    Add Button

---
+++ components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React, { FC, ReactNode } from 'react';
+import styles from './Button.module.scss';
+
+interface ButtonProps {
+  children: ReactNode;
+}
+
+const Button: FC<ButtonProps> = ({ children }) => {
+  return <button className={styles.btn}>{children}</button>;
+};
+
+export default Button;
