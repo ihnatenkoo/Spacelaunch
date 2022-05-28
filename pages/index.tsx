5aba9f7c16a9d38e9a1e75c63a2f85f674b70969
Dmytro <ihnatenko.dev@gmail.com>
2022-05-28 15:18:29 +0300

    Add scroll event in Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,41 @@ import type { GetStaticProps, NextPage } from 'next';
 import { LaunchesRequestData, HomeProps } from '../Interfaces';
 import { MainLayout } from '../layout/';
 import { HomeIntro, HomeLaunches } from '../components';
+import { useEffect, useState } from 'react';
 
 const Home: NextPage<HomeProps> = ({ launchesData }) => {
+  const initialLaunchesData = launchesData.slice(0, 6);
+
+  const [data, setData] = useState(initialLaunchesData);
+  const [offset, setOffset] = useState(12);
+  const [newData, setNewData] = useState(false);
+
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 200) {
+      setNewData(true);
+    }
+  };
+
+  useEffect(() => {
+    if (newData) {
+      const newData = launchesData.slice(0, offset);
+      setData(newData);
+      setOffset((offset) => offset + 6);
+      console.log(newData);
+    }
+    setNewData(false);
+  }, [newData]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', event);
+    return () => window.removeEventListener('scroll', event);
+  }, []);
+
   return (
     <MainLayout header="homepage">
       <HomeIntro />
       <div className="container fill">
-        <HomeLaunches launchesData={launchesData} />
+        <HomeLaunches launchesData={data} />
       </div>
     </MainLayout>
   );
@@ -19,7 +47,7 @@ export default Home;
 
 export const getStaticProps: GetStaticProps = async () => {
   const { data } = await axios.get(
-    'https://spacelaunchnow.me/api/3.3.0/launch/upcoming?mode=detailed&limit=6&offset=0'
+    `https://spacelaunchnow.me/api/3.3.0/launch/upcoming?mode=detailed&limit=96&offset=0`
   );
 
   const launchesData = data.results.map((item: LaunchesRequestData) => {
@@ -35,6 +63,7 @@ export const getStaticProps: GetStaticProps = async () => {
   });
 
   return {
-    props: { launchesData }
+    props: { launchesData },
+    revalidate: 43200
   };
 };
