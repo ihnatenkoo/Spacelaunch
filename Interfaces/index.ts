e316e8eb8ae0f266757dd8ec1086a9e9cd7a8976
Dmytro <ihnatenko.dev@gmail.com>
2022-06-05 19:59:22 +0300

    Add utils transformSingleLaunchData

--- Interfaces/index.ts
+++ Interfaces/index.ts
@@ -4,6 +4,25 @@ export interface LaunchesData {
   id: string;
   date: Date;
 }
+export interface SingleLaunchData {
+  name: string;
+  image: string;
+  type: string;
+  orbit: string;
+  location: string;
+  launchComplex: string;
+  rocketName: string;
+  missionDescr: string;
+  rocketFamily: string;
+  rocketVariant: string;
+  rocketDescr: string;
+  rocketLink: string;
+  rocketWiki: string;
+  eventCoordinates: { lat: number; lng: number };
+  rocketId: string;
+  date: string;
+}
+
 export interface HomeProps {
   staticLaunchesData: Array<LaunchesData>;
 }
---
+++ utils/index.ts
@@ -0,0 +1,60 @@
+import { LaunchesData, SingleLaunchData } from '../Interfaces';
+
+export const transformLaunchesData = (data: any): Array<LaunchesData> => {
+  const transformData = data.map((item: any) => {
+    const { id, name, net: date } = item;
+    const { image_url: image } = item.rocket.configuration;
+
+    return {
+      name,
+      image,
+      id,
+      date
+    };
+  });
+
+  return transformData;
+};
+
+export const transformSingleLaunchData = (data: any): SingleLaunchData => {
+  const { name, net: date } = data;
+  const { latitude, longitude, name: launchComplex } = data.pad;
+  const { name: location } = data.pad.location;
+  const {
+    type = 'No info',
+    orbit = 'No info',
+    description: missionDescr = 'Description not available'
+  } = data.mission || {};
+
+  const {
+    id: rocketId,
+    image_url: image,
+    name: rocketName,
+    family: rocketFamily,
+    variant: rocketVariant,
+    description: rocketDescr,
+    info_url: rocketLink,
+    wiki_url: rocketWiki
+  } = data.rocket.configuration;
+
+  const singleLaunchData = {
+    name,
+    image,
+    date,
+    type,
+    orbit,
+    location,
+    launchComplex,
+    rocketName,
+    missionDescr,
+    rocketFamily,
+    rocketVariant,
+    rocketDescr,
+    rocketLink,
+    rocketWiki,
+    eventCoordinates: { lat: +latitude, lng: +longitude },
+    rocketId
+  };
+
+  return singleLaunchData;
+};
