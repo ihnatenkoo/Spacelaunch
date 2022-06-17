c1f5ac43bb9d8bb841dc029b516445bf3e6e8442
Dmytro <ihnatenko.dev@gmail.com>
2022-06-17 17:19:43 +0300

    Add favicon, apple-touch-icons and main meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,14 +4,71 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="author" content="Dmytro Ihnatenko" />
+        <meta httpEquiv="Reply-to" content="ihnatenko.dev@gmail.com" />
+        <meta name="robots" content="index, follow" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,400;0,600;0,700;0,800;0,900;1,500&family=Roboto:ital,wght@0,400;0,700;1,500&display=swap"
         />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
+
+        <meta name="application-name" content="Space Launch" />
+        <meta name="msapplication-tooltip" content="SPA application for Spacelaunch api" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="mobile-web-app-capable" content="yes" />
+
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-touch-icon/apple-touch-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-touch-icon/apple-touch-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-touch-icon/apple-touch-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-touch-icon/apple-touch-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-touch-icon/apple-touch-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-touch-icon/apple-touch-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-touch-icon/apple-touch-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon/apple-touch-icon-180x180.png"
+        />
+
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="msapplication-TileColor" content="#181b48" />
+        <meta name="msapplication-tap-highlight" content="no" />
       </Head>
       <body>
         <Main />
