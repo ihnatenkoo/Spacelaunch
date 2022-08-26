21e6246fe33a530617dbc9b47b8c8847e71d3721
Dmytro <ihnatenko.dev@gmail.com>
2022-08-26 19:43:46 +0300

    Install progressbar and add  in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,21 @@
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import NextNProgress from 'nextjs-progressbar';
 import { Provider } from 'react-redux';
-import store from '../redux';
 
+import store from '../redux';
 import '../styles/globals.scss';
 
-import type { AppProps } from 'next/app';
-
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Provider store={store}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Component {...pageProps} />
-    </Provider>
-  );
+	return (
+		<Provider store={store}>
+			<Head>
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			<NextNProgress color="#6d28d9" height={4} />
+			<Component {...pageProps} />
+		</Provider>
+	);
 }
 
 export default MyApp;
