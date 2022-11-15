29484e96d61faef81418976e0ca4e07e991fb338
Dmytro <ihnatenko.dev@gmail.com>
2022-11-15 16:18:19 +0200

    Fix next.config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,28 @@
 const withPWA = require('next-pwa');
 
 module.exports = withPWA({
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    });
+	reactStrictMode: true,
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ['@svgr/webpack'],
+		});
 
-    return config;
-  },
+		return config;
+	},
 
-  images: {
-    domains: ['spacelaunchnow-prod-east.nyc3.cdn.digitaloceanspaces.com']
-  },
+	images: {
+		domains: [
+			'spacelaunchnow-prod-east.nyc3.cdn.digitaloceanspaces.com',
+			'spacelaunchnow-prod-east.nyc3.digitaloceanspaces.com',
+			'nyc3.digitaloceanspaces.com',
+		],
+	},
 
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
-  }
+	pwa: {
+		dest: 'public',
+		register: true,
+		skipWaiting: true,
+		disable: process.env.NODE_ENV === 'development',
+	},
 });
